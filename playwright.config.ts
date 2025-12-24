4765908eeaea2337e560cc3c9824929090a83cbf
bdelatorre8 <bdelatorre8@MacBook-Pro.local>
2025-12-23 19:53:54 -0600

    fix(e2e): utilize vite preview in CI for stability

--- playwright.config.ts
+++ playwright.config.ts
@@ -42,7 +42,9 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'npm run dev',
+    command: process.env.CI
+      ? 'cd frontend && npm run build && npm run preview -- --port 5173'
+      : 'npm run dev',
     url: 'http://localhost:5173',
     reuseExistingServer: !process.env.CI,
     timeout: 120 * 1000,
